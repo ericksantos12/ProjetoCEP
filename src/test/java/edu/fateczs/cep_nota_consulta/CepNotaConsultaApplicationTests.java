d47597c8b43d80da1b1f1d48d357ae69e3564b60
Erick Sousa <erick.sousa@hotmail.com.br>
2023-05-22 15:54:03 -0300

    :tada: big bang

---
+++ src/test/java/edu/fateczs/cep_nota_consulta/CepNotaConsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fateczs.cep_nota_consulta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepNotaConsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
