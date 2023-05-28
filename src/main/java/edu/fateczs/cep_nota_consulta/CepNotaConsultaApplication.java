00d70a1734c9689d84bf39bef88f7d9d5d7490e2
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-05-28 01:19:54 -0300

    ✨ new CepController for routes management
    Co-authored-by: Guilherme-Soares-Sousa <Guilherme-Soares-Sousa@users.noreply.github.com>
    Co-authored-by: Gabrielle Carvalho <GabrielleCGNeves@users.noreply.github.com>

--- src/main/java/edu/fateczs/cep_nota_consulta/CepNotaConsultaApplication.java
+++ src/main/java/edu/fateczs/cep_nota_consulta/CepNotaConsultaApplication.java
@@ -2,18 +2,9 @@ package edu.fateczs.cep_nota_consulta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
 public class CepNotaConsultaApplication {
-
-	@GetMapping("/")
-	public String index(){
-		return "index.html";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CepNotaConsultaApplication.class, args);
---
+++ src/main/java/edu/fateczs/cep_nota_consulta/controller/CepController.java
@@ -0,0 +1,42 @@
+package edu.fateczs.cep_nota_consulta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import edu.fateczs.cep_nota_consulta.CepObjeto;
+import edu.fateczs.cep_nota_consulta.service.ApiService;
+
+@Controller
+public class CepController {
+	private ApiService apiService;
+
+	public CepController(ApiService apiService) {
+		this.apiService = apiService;
+	}
+
+    @GetMapping("/")
+	public String index(){
+		return "index";
+	}
+
+	@GetMapping("/notas")
+	public String notas(){
+		return "notas";
+	}
+
+	@GetMapping("/cep")
+	public String cep(ModelMap model){
+		model.addAttribute("cepObjeto", new CepObjeto());
+		return "cep";
+	}
+	@PostMapping("/cep")
+	public String cep(@ModelAttribute CepObjeto cepObjeto, ModelMap model){
+		String request = apiService.fazerRequisicaoCEP(cepObjeto.getCep());
+		model.addAttribute("request", request);
+		return "cep";
+	}
+
+}
