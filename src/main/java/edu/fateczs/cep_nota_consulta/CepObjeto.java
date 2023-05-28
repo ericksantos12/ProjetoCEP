e187b2998fd31863064a0658573e2cb360c66ffb
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-05-28 01:28:36 -0300

    ✨ CEP API Request forms via `ViaCep`
    Co-authored-by: Guilherme-Soares-Sousa <Guilherme-Soares-Sousa@users.noreply.github.com>
    Co-authored-by: Gabrielle Carvalho <GabrielleCGNeves@users.noreply.github.com>

---
+++ src/main/java/edu/fateczs/cep_nota_consulta/CepObjeto.java
@@ -0,0 +1,14 @@
+package edu.fateczs.cep_nota_consulta;
+
+public class CepObjeto {
+    private String cep;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    
+}
